b0a1fcade883fdc4a1d0bbb1023a4fc27544a2e4
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-27 12:55:31 +0800

    upated script test

--- .buildkite/trigger-pr-pipeline-script.sh
+++ .buildkite/trigger-pr-pipeline-script.sh
@@ -7,7 +7,7 @@ result="$(buildkite-agent step get outcome --step filter-md-script)"
 buildkite-agent pipeline upload <<YAML
 steps:
   - label: "The pr pipeline"
-    command: ".buildkite/pr-pipeline.yml"
+    command: .buildkite/pr-pipeline.yml
     build:
       env:
         RESULT: "${result}"
