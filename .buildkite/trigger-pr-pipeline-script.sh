1bd5cb25bbd4327e3550cb0fb10165ab931b843a
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-27 14:03:10 +0800

    asd

--- .buildkite/trigger-pr-pipeline-script.sh
+++ .buildkite/trigger-pr-pipeline-script.sh
@@ -8,6 +8,10 @@ buildkite-agent pipeline upload <<YAML
 steps:
   - label: "The pr pipeline"
     trigger: "firstproject"
+    skip:  if [ $(buildkite-agent step get "outcome" --step "one") == "soft_failed" ]; then
+        buildkite-agent annotate 'this build is skip' --style 'error'
+        "skip"
+      fi
     build:
       env:
         RESULT: "${result}"
