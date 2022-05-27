8901e2bd9ea6e634a86ce1eafce75ceac2c93f3e
unknown <joeffrey.chaucer.paitan@gmail.com>
2022-05-27 14:42:25 +0800

    updated script

--- .buildkite/trigger-pr-pipeline-script.sh
+++ .buildkite/trigger-pr-pipeline-script.sh
@@ -4,16 +4,14 @@ set -euo pipefail
 
 result="$(buildkite-agent step get outcome --step filter)"
 
+
+if [ $result == "passed" ]; then
 buildkite-agent pipeline upload <<YAML
 steps:
   - label: "The pr pipeline"
     trigger: "firstproject"
-    skip:  if [ $result != "passed" ]; then
-        true
-        else
-        false
-      fi
     build:
       env:
         RESULT: "${result}"
 YAML
+fi
